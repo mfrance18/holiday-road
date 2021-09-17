2e6ea5c51550e6b786b94c9880f3d5ce3282f767
Matt F <trumpetman072392@gmail.com>
2021-09-17 18:36:17 -0500

    added settings.js and db.json

---
+++ scripts/Settings.js
@@ -0,0 +1,5 @@
+export const settings = {
+	graphhopperKey: "5054c436-5c6d-4f0c-b6bd-d8c8fd7d93d1",
+	npsKey: "62KxgddQg7QpiMHlIB0FdCbquACUmSuMVJuffLz1",
+	weatherKey: "9f216fcc9206f98d20b2f1d761fb9674"
+};
